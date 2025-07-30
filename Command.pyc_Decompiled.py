aa5142a0631c8c7f38356d5d44b8587d88699cb2
Kim <iyai@naver.com>
2025-07-30 20:24:51 +0900

    Add files via upload

---
+++ Command.pyc_Decompiled.py
@@ -0,0 +1,24 @@
+# Decompiled with PyLingual (https://pylingual.io)
+# Internal filename: Command.py
+# Bytecode version: 3.8.0rc1+ (3413)
+# Source timestamp: 1970-01-01 00:00:00 UTC (0)
+
+class COMMAND:
+    CMD_MOTOR = 'CMD_MOTOR'
+    CMD_M_MOTOR = 'CMD_M_MOTOR'
+    CMD_CAR_ROTATE = 'CMD_CAR_ROTATE'
+    CMD_LED = 'CMD_LED'
+    CMD_LED_MOD = 'CMD_LED_MOD'
+    CMD_SERVO = 'CMD_SERVO'
+    CMD_BUZZER = 'CMD_BUZZER'
+    CMD_SONIC = 'CMD_SONIC'
+    CMD_LIGHT = 'CMD_LIGHT'
+    CMD_POWER = 'CMD_POWER'
+    CMD_CLOSE = 'CMD_CLOSE'
+    CMD_LED_TYPE = 'CMD_LED_TYPE'
+    CMD_START = 'Start'
+    CMD_STOP = 'Stop'
+    CMD_MODE = 'CMD_MODE'
+
+    def __init__(self):
+        return
